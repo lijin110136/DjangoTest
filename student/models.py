41de95ea78acd8a2a49956aec3e445c6c6c8bfac
lijin <lijin@dianxinos.com>
2014-04-14 17:52:12 +0800

    修改模型

--- student/models.py
+++ student/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    id = models.IntegerField(primary_key=True)
+    sname = models.CharField(max_length=100)
+    sage = models.IntegerField(max_length=10)
+
+    def __str__(self):
+        return self.sname
+
