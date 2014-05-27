8a566b338f069ce01139f7ab6df02b1b7317cb12
lijin <lijin@dianxinos.com>
2014-05-27 17:41:27 +0800

    新增学生列表删除操作

--- student/views.py
+++ student/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.db.models import Q
+from django.http import HttpResponseRedirect
 from models import Student
 # Create your views here.
 def search(request):
@@ -14,7 +15,13 @@ def search(request):
     else:
         results = []
     return render_to_response('search/search.html', {"results": results, "query": query})
+
 def list(request):
-    results = Student.objects.distinct()
+    results = Student.objects.distinct().order_by('id')
     return render_to_response("student/list.html", {"lists" : results},
                                context_instance=RequestContext(request))
+
+def delete(request, id):
+    student = Student.objects.get(id=id)
+    student.delete()
+    return HttpResponseRedirect('/myapp/student/list')
